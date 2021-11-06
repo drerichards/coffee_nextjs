ef0485ffc183a0bdfe5d046921a4566235e9d835
Andre <g.andrerichards@gmail.com>
2021-11-06 01:45:00 -0400

    add card comp

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Banner from "../components/banner"
+import Card from "../components/card"
 import styles from '../styles/Home.module.css'
 
 export default function Home () {
   const handleBannerBtnClick = e => {
-    console.log(e);
+    console.log(e)
   }
 
   return (
     <div className={styles.container}>
-    {/* tells browser and search engine all the technical info about the web page */}
+      {/* tells browser and search engine all the technical info about the web page */}
       <Head>
         <title>Coffee Connoisseur</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Banner buttonText="View stores nearby" handleClick={handleBannerBtnClick}/>
+        <Banner buttonText="View stores nearby" handleClick={handleBannerBtnClick} />
         <div className={styles.heroImage}>
-        <Image src="/static/hero-image.svg" alt="Hero Image" width={800} height={500} />
+          <Image src="/static/hero-image.svg" alt="Hero Image" width={800} height={500} />
         </div>
+        <Card name='Coffee' href='/coffee-shop/coffee' img='/hero-image.svg' />
       </main>
     </div>
   )
